d61858eee751842191e8b7f8945e88775669384e
NazmiAF11 <114555528+NazmiAF11@users.noreply.github.com>
2024-06-09 09:42:55 +0700

    Add files via upload

---
+++ nazmi/latihan.js
@@ -0,0 +1,24 @@
+function createXMLHttppObject() {
+    var xmlhttp = false;
+    try {
+    var xmlhttp = new ActiveXObject ("Msxm12.XMLHTTP");
+} catch (e) {
+    try {
+        xmlhttp = new ActiveXObject ("Microsoft.XMLHTTP");
+    } catch (E) {
+        xmlhttp = false ;
+    }
+}
+
+    if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
+    xmlhttp = new XMLHttpRequest () ;
+}
+
+    if (!xmlhttp) {
+    alert("Terjadi kesalahan saat pembuatan XMLHttpRequest object!");
+}   else {
+    alert("XMLHttpRequest berhasil dibuat.");
+}
+
+    return xmlhttp;
+}
